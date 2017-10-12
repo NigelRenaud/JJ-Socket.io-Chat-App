f17e8e528a5adafa05480182eece90db3b342f6d
Darren Nesbitt <darrencnesbitt@gmail.com>
2017-10-12 15:29:13 -0400

    rewritting required modules based on socket.io docs, adding 'http' and 'app'

--- jjapp/server.js
+++ jjapp/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 // initialize the app
 const app = express();
+// importing the socket that allows us to get chat messages instantly
+const io = require('socket.io')
 // set the port, either from an environmental variable or manually
 const port = process.env.PORT || 3000;
 
@@ -9,4 +11,10 @@ const port = process.env.PORT || 3000;
 // tell the app where to serve
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
+});
+
+
+// index route
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
